0f4b3b76f4325c70b25b3641ab3d1af2862a7825
Mikhail Kotlik <mkotlik@stuy.edu>
2016-05-17 12:32:44 -0400

    outlined/worked on SocketHandler and VersaServer

---
+++ SocketHandler.java
@@ -0,0 +1,47 @@
+/* Copyright (c) 2016, Mikhail Kotlik and Sam Xu
+ * Versa Checkers
+ * APCS Spring Final Project
+ * VersaServer
+ */
+
+import java.net.*;
+import java.io.*;
+
+public class SocketHandler implements Runnable{
+    //Instance variables
+    boolean listening;
+    int portNumber;
+    ThreadLinker mainLink;
+
+    //Constructors
+    public SocketHandler(int portNumber, ThreadLinker mainLink) {
+        this.listening = true;
+        this.portNumber = portNumber;
+        this.mainLink = mainLink;
+        new Thread(this, 'SOCKET_HANDLER').start();
+    }
+
+    //Run
+    public void run() {
+        try (ServerSocket mainSocket = new ServerSocket(portNumber)) {
+            System.out.println("SOCKET_HANDLER: Socket started on port " + portNumber);
+            System.out.println("SOCKET_HANDLER: Listening for connections");
+            while (listening) {
+                /*
+                 * Check for new messages from serverMain
+                 * Respond to messages from serverMain
+                 */
+                /*
+                 * Assign temp threadID to new ClientHandler
+                 * Create new ThreadLinker with main id and clienthandler id
+                 * Create new ClientHandler with this threadLinker
+                 * Add threadLinker to main's hashMap of threadLinkers
+                 */
+            }
+            System.out.println("SOCKET_HANDLER: Stopped listening. Socket closing.");
+        } catch (IOException e) {
+            System.err.println("SOCKET_HANDLER: IOException - Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
--- VersaServer.java
+++ VersaServer.java
@@ -6,26 +6,30 @@
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 
 public class VersaServer {
-    public static void main(String[]args) {
-        boolean listening = true;
-        //default port number is 1216 (TCP-registered but by some unknown entity)
-        int portNumber = 1216;
-        int clientCount = 1;
-        try (ServerSocket mainSocket = new ServerSocket(portNumber)) {
-            System.out.println("Socket started on port " + portNumber);
-            System.out.println("Listening for connections");
-            while (listening) {
-                new Thread(new ClientHandler(mainSocket.accept(), clientCount)).start();
-                System.out.println("Connected client #" + clientCount);
-                clientCount++;
-            }
-            System.out.println("Listen loop ended");
-        } catch (IOException e) {
-            System.err.println("Could not listen on port " + portNumber);
-            System.exit(-1);
-        }
+    /*
+     * Spawns new hostIO thread
+     * Spawns new SocketHandler thread
+     * In loop:
+     * Checks for new hostIO messages
+     * Responds to hostIO messages
+     * Checks for new SocketHandler messages
+     * Responds to SocketHandler messages
+     */
+
+    //Main
+    public static void main(String[] args) {
+
     }
+
+    //Instance variables
+
+    //Constructors
+
+    //Accessors
+
+    //Mutators
 }
