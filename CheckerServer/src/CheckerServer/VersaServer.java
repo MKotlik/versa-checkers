79e1aba436539666f744c0765318a23cebb0d8ae
Sam Xu <yxu8@stuy.edu>
2016-05-23 23:55:03 -0400

    Finshes a crapton of stuff

--- CheckerServer/src/CheckerServer/VersaServer.java
+++ CheckerServer/src/CheckerServer/VersaServer.java
@@ -35,11 +35,46 @@ public class VersaServer extends Thread{
         max_connections = 10;
     }
 
-    public int addClient(){
-
+    public int addClient(VersaServerThread client){
+        if(clients.containsKey(client.name)){
+            System.err.println("Please choose another name");
+            return 2;
+        }else if(num_connected >= max_connections){
+            System.err.println("Connections full");
+            return 1;
+        }else{
+            clients.put(client.name, client);
+            num_connected += 1;
+            sendClientList();
+            return 0;
+        }
     }
 
-    public void removeClient(VersaServerThread){
+    public void removeClient(VersaServerThread client){
+        if(clients.containsKey(client.name)){
+            clients.remove(client.name);
+            num_connected -= 1;
+            sendClientList();
+        }else{
+            System.err.println("Unable to remove client, are you sure it exist?");
+        }
+    }
 
+    private void sendClientList(){
+        String names = "###name_list=";
+        String [] clientIPList = new String[clients.size()];
+        VersaServerThread[] clientsList = (VersaServerThread[]) clients.values().toArray(new VersaServerThread[0]);
+        for(int n = 0; n < clientsList.length; n++) {
+            clientIPList[n] = String.valueOf(clientsList[n].getSocket().getRemoteSocketAddress()) + " " + clientsList[n].name;
+            names += clientsList[n].name + ",";
+        }
+        names = names.substring(0, names.length()-1) + "###";
+        gui.writeClientList(clientIPList);
+
+        for(int n = 0; n < clientsList.length; n++) {
+            if (clientsList[n] != null) {
+                clientsList[n].sendMessage("server", names);
+            }
+        }
     }
 }
--- CheckerServer/src/CheckerServer/VersaServerThread.java
+++ CheckerServer/src/CheckerServer/VersaServerThread.java
@@ -30,17 +30,33 @@ public class VersaServerThread extends Thread{
         this.socket = socket;
 
         try{
-            //To be completed
+            //To be completed this.in = in =
+            this.out = new PrintWriter(socket.getOutputStream(),true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         }catch(IOException e){
             System.err.println("Unable to initialize I/O for the thread");
         }
     }
 
-    public static class Packet{
+    public void sendMessage(String sender, String incMessage){
+        out.println("###sentfrom=" + sender + "###message" + incMessage);
+    }
+
+    //Kills the thread
+    public void kill(){
+        listening = false;
+    }
+
+    //accessor method
+    public Socket getSocket(){
+        return this.socket;
+    }
+
+    public static class Message{
         public String recip;
         public String content;
 
-        public Packet(String message){
+        public Message(String message){
             recip = message.substring(message.indexOf("###sendto")+10,message.indexOf("###message="));
             content = message.substring(message.indexOf("###message")+11,message.length());
         }
@@ -48,6 +64,66 @@ public class VersaServerThread extends Thread{
         @Override
         public String toString(){
             return "Recipient: " + recip + " Message: " + content;
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null && listening) {
+                Message message = new Message(inputLine);
+                if (message.recip.equals("server")) {
+                    if (message.content.equals("###disconnecting###")) {
+                        server.sendMessage("all", name, "###potential_chat_disconnected###");
+                        break;
+                    }
+                    else if (message.content.contains("###name=")) {
+                        name = message.content.substring(message.content.indexOf("=")+1, message.content.length()-3);
+
+                        int res = server.addClient(this);
+                        if (res == 0) {
+                            ready = true;
+                        } else if (res == 1) {
+                            sendMessage("server", "###too_many_connections###");
+                            break;
+                        } else if (res == 2) {
+                            sendMessage("server", "###name_already_taken###");
+                            break;
+                        }
+                    }
+                } else {
+                    if (ready) {
+                        if (message.content.equals("###new_game_window###")) {
+                            server.newGame(name, message.recip);
+                        }
+                        else if (message.content.contains("###new_move")) {
+                            server.gameMove(name, message.recip, message.content);
+                        }
+                        else if (message.content.equals("###game_over###you_win###")) {
+                            server.endGame(name, message.recip);
+                        }
+                        else if (message.content.equals("###new_game_restarted###")) {
+                            server.restartGame(name, message.recip);
+                        }
+
+                        else {
+                            server.sendMessage(message.recip, name, message.content);
+                        }
+                    }
+                }
+            }
+
+            out.close();
+            in.close();
+            socket.close();
+            if (ready) {
+                server.removeClient(this);
+            }
+
+        } catch (IOException e) {
+
         }
     }
 }
