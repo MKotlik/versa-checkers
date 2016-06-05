ff1b912a91d74b4695d5252ad987cc6bec79615d
Sam Xu <yxu8@stuy.edu>
2016-06-04 20:17:03 -0400

    Configures bot for sever

--- CheckerClient/src/CheckerClient/VersaBot.java
+++ CheckerClient/src/CheckerClient/VersaBot.java
@@ -6,44 +6,78 @@ package CheckerClient;
  * VersaClientChat
  */
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.util.HashMap;
 
-public class VersaBot extends VersaClient{
+public class VersaBot extends VersaClientGUI{
+    private VersaClient client = null;
+    public HashMap chats = null;
+    private String name;
+    private int portNum = 0;
+    private String ip = "";
 
-    private VersaClientGUI gui = null;
-    private Socket clientSocket = null;
-    private PrintWriter out = null;
-    private BufferedReader in = null;
-    private boolean listening = true;
+    public VersaBot(String name, int port, String IP){
+        chats = new HashMap();
 
-    public VersaBot(VersaClientGUI gui){
-        super(gui);
+        this.name = name;
+        portNum = port;
+        ip = IP;
     }
 
-    public int evaluateBoard(String board, boolean player){
-        int value = 0;
+    private void connectClient() {
+        String IPAddress = ip;
+        int port = portNum;
 
-        if (player){
-            value = evaluateForPlayer(board);
-        }else{
-            value = evaluateForBot(board);
+
+        VersaClient c = new VersaClient(this);
+        if (c.connect(IPAddress, port, name) == 1) {
+            this.client = c;
+
+            this.client.start();
+        } else {}
+    }
+
+    public void setUserList(String[] names) {}
+
+    public void notifyWin(String partner){}
+
+    public void disconnect(String reason) {
+        if (client.disconnect() == 1) {
+            if (!chats.isEmpty()) {
+                for (VersaClientChat chat : (VersaClientChat[]) chats.values().toArray(new VersaClientChat[0])) {
+                    chat.disconnect(reason);
+                }
+            }
+            client = null;
+            name = "";
         }
-
-        return value;
     }
 
-    public int evaluateForPlayer(String board){
-        int value = 0;
-        return value;
+    public void newChatWindow(String partner, boolean auto) {
+        if (!chats.containsKey(partner)) {
+            final VersaClientChat newChat = new VersaClientChat(partner, client, this);
+            chats.put(partner, newChat);
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {}
+            });
+        }else {
+            System.err.println("A game with this person is already open");
+        }
     }
 
-    public int evaluateForBot(String board){
-        int value = 0;
-        return value;
+    public void recievedMessage(String sender, String message) {
     }
 
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new VersaBot("Bot1234",1216,"127.0.0.1").connectClient();
+            }
+        });
+    }
 
 
 }
